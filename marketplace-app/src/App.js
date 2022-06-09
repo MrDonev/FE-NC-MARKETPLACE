5f558012b1efe5c6a7653c85aeb4e6b04975eeb8
Iliya <mrdonev@gmail.com>
2022-06-09 11:59:30 +0100

    layed down the structure of the website

--- marketplace-app/src/App.js
+++ marketplace-app/src/App.js
@@ -1,11 +1,21 @@
-import "./App.css";
-import Main from "./components/main";
-
+import './App.css';
+import Header from './components/Header.jsx';
+import Main from './components/Main';
+import Nav from './components/Nav';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Profile from './components/Profile';
 function App() {
   return (
-    <>
-      <Main />
-    </>
+    <BrowserRouter>
+    <section className='App'>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Main/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+      <Nav />
+    </section>
+    </BrowserRouter>
   );
 }
 
---
+++ marketplace-app/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import Main from "./Main";
+import Profile from "./Profile";
+
+const Nav = () => {
+  return (
+    <section className="Nav">
+      <h2>Nav</h2>
+    
+
+    </section>
+  );
+};
+
+export default Nav;
