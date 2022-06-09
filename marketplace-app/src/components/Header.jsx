e637fac28703a5bfbfaa2e229cacf5cf01272ef2
Iliya <mrdonev@gmail.com>
2022-06-09 23:32:35 +0100

    added some css styling

--- marketplace-app/src/components/Header.jsx
+++ marketplace-app/src/components/Header.jsx
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom';
+import BasketIcon from '../Assets/basket.webp'
+import ProfileIcon from '../Assets/profile.png'
+import HomeIcon from '../Assets/home.png'
 const Header = () => {
   return (
     <section className="Header">
       <h1>D & T`s marketplace</h1>
       <section className="headerLinks">
-        <Link to={'/'}>Home</Link>
-        <Link to={'/profile'}>Profile</Link>
-        <Link to={'/basket'}>Basket</Link>
+        <Link  to={'/'}><img id='home' src={HomeIcon} alt='home'/></Link>
+        <Link  to={'/profile'}><img id='profile' src={ProfileIcon} alt='profile'/></Link>
+        <Link  to={'/basket'}><img id='basket' src={BasketIcon} alt='basket'/></Link>
       </section>
     </section>
   );
